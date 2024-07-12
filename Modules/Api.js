59ba607fd1e98e1f7ada7b51ec80e7ac1f18ddce
Ayoub KHEMISSI <akhemissi@norsys.fr>
2024-07-13 01:59:18 +0200

    feat: api instance

--- Modules/Api.js
+++ Modules/Api.js
@@ -2,6 +2,7 @@ import consoleStamp from "console-stamp";
 import express from "express";
 import { readFileSync } from "fs";
 import Config from "../Utils/Config.js";
+import Database from "./Database.js";
 
 consoleStamp(console, { format: ":date(yyyy-mm-dd HH:MM:ss.l):label" });
 
@@ -18,6 +19,10 @@ api.get("/", function (req, res) {
 
 api.listen(apiPort, apiHostname, function () {
     console.info(`API listening on http${apiHttpSecure ? "s" : ""}://${apiHostname}:${apiPort}/.`);
+
+    const values = [Date.now(), version];
+    const query = "INSERT INTO api_instance (started_at, version) VALUES (?, ?)";
+    Database.execute(query, values);
 });
 
 export default api;
