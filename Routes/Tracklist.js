2a9facd6d5607e82573451102a96ec0ea847daa1
Ayoub KHEMISSI <akhemissi@norsys.fr>
2024-10-12 06:00:24 +0200

    feat: Tracklist data reworked - first_normal_price & first_discounted_price added to each track - the first track check in the track_checks array is the last track check recorded

--- Routes/Tracklist.js
+++ Routes/Tracklist.js
@@ -20,15 +20,15 @@ api.get("/tracklist", async function (req, res) {
     }
 
     const valuesA = [jwt.id];
-    const queryA = "SELECT id, url, name, description, currency, additional_info, track_stock, track_price, track_price_threshold, status_id, created_at, updated_at FROM track WHERE user_id=?";
+    const queryA = "SELECT t.id, t.url, t.name, t.description, t.currency, t.additional_info, t.track_stock, t.track_price, t.track_price_threshold, t.status_id, t.created_at, t.updated_at, (SELECT tc.normal_price FROM track_check tc WHERE tc.track_id = t.id ORDER BY tc.created_at ASC LIMIT 1) AS first_normal_price, (SELECT tc.discounted_price FROM track_check tc WHERE tc.track_id = t.id ORDER BY tc.created_at ASC LIMIT 1) AS first_discounted_price FROM track t WHERE t.user_id = ?";
     const [resultA] = await Database.execute(queryA, valuesA);
 
     for (const track of resultA) {
         const valuesB = [track.id, track.id];
-        const queryB = "SELECT tc.id, tc.created_at, tc.discounted_price AS discounted_price, tc.normal_price AS normal_price, tc.availability AS availability FROM track_check tc JOIN (SELECT id, created_at, discounted_price, normal_price, availability, LAG(discounted_price) OVER (ORDER BY created_at) AS prev_discounted_price, LAG(normal_price) OVER (ORDER BY created_at) AS prev_normal_price, LAG(availability) OVER (ORDER BY created_at) AS prev_availability FROM track_check WHERE created_at >= UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 6 MONTH)) * 1000 AND track_id=?) ptc ON tc.id = ptc.id WHERE (tc.discounted_price <> ptc.prev_discounted_price OR tc.normal_price <> ptc.prev_normal_price OR tc.availability <> ptc.prev_availability) AND tc.track_id=? ORDER BY tc.created_at ASC;";
+        const queryB = "SELECT tc.id, tc.created_at, tc.discounted_price AS discounted_price, tc.normal_price AS normal_price, tc.availability AS availability FROM track_check tc JOIN (SELECT id, created_at, discounted_price, normal_price, availability, LAG(discounted_price) OVER (ORDER BY created_at) AS prev_discounted_price, LAG(normal_price) OVER (ORDER BY created_at) AS prev_normal_price, LAG(availability) OVER (ORDER BY created_at) AS prev_availability FROM track_check WHERE created_at >= UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 6 MONTH)) * 1000 AND track_id=?) ptc ON tc.id = ptc.id WHERE (tc.discounted_price <> ptc.prev_discounted_price OR tc.normal_price <> ptc.prev_normal_price OR tc.availability <> ptc.prev_availability) AND tc.track_id=? ORDER BY tc.created_at DESC";
         const [resultB] = await Database.execute(queryB, valuesB);
 
-        track.trackChecks = [...resultB];
+        track.track_checks = [...resultB];
     }
 
     res.status(200).json({ data: resultA, msg: "Tracklist successfully found." });
