192d415a78888a43ca6403f4398518dd9e692f50
Ayoub KHEMISSI <akhemissi@norsys.fr>
2024-10-29 17:47:54 +0100

    fix: Account delete route all tracks are set to deleted (not disabled)

--- Routes/AccountDelete.js
+++ Routes/AccountDelete.js
@@ -3,7 +3,7 @@ import { extractJwt, verifyAuthJwt } from "../Modules/Auth.js";
 import Database from "../Modules/Database.js";
 import Constants from "../Utils/Constants.js";
 
-const { subscriptionActive, trackStatusDisabled } = Constants;
+const { subscriptionActive, trackStatusDeleted } = Constants;
 
 api.delete("/account/delete/", async function (req, res) {
     const jwt = verifyAuthJwt(extractJwt(req.cookies));
@@ -31,7 +31,7 @@ api.delete("/account/delete/", async function (req, res) {
         return;
     }
 
-    const valuesB = [trackStatusDisabled, jwt.id];
+    const valuesB = [trackStatusDeleted, jwt.id];
     const queryB = "UPDATE track SET status_id=? WHERE user_id=?";
     await Database.execute(queryB, valuesB);
 
