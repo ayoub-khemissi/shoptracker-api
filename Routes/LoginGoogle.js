e779ec2c6809a7ec19ec1cd3c9ee6b32d6b24b10
Ayoub KHEMISSI <akhemissi@norsys.fr>
2024-12-27 18:14:55 +0100

    fix: Login Google Route remove register if user not found

--- Routes/LoginGoogle.js
+++ Routes/LoginGoogle.js
@@ -29,90 +29,47 @@ api.post("/login/google", async function (req, res) {
     const queryA = "SELECT * FROM user WHERE email=?";
     const [resultA] = await Database.execute(queryA, valuesA);
 
-    let data = null;
-    let jwt = null;
     if (resultA.length === 0) {
-        const valuesB = [email, true, false, true, true, Date.now()];
-        const queryB =
-            "INSERT INTO user (email, alert_email, alert_text, alert_browser_notification, alert_push_notification, created_at) VALUES (?, ?, ?, ?, ?, ?)";
-        const [resultB] = await Database.execute(queryB, valuesB);
-
-        if (resultB.affectedRows === 0) {
-            res.status(400).json({ data: null, msg: "User not inserted." });
-            return;
-        }
-
-        const valuesC = [resultB.insertId];
-        const queryC = "SELECT * FROM user WHERE id = ?";
-        const [resultC] = await Database.execute(queryC, valuesC);
-
-        if (resultC.length === 0) {
-            res.status(404).json({ data: null, msg: "User not found." });
-            return;
-        }
-
-        const user = resultC[0];
-
-        const valuesD = [user.id, subscriptionActive];
-        const queryD = "SELECT p.stripe_price_id, s.stripe_subscription_id FROM subscription s, plan p WHERE s.user_id=? AND s.status_id=? AND s.plan_id=p.id";
-        const [resultD] = await Database.execute(queryD, valuesD);
-
-        if (resultD.length > 0) {
-            user.subscription = resultD[0];
-
-            const subscriptionDetails = await getSubscriptionDetails(user.subscription.stripe_subscription_id);
-            user.subscription = { ...user.subscription, ...subscriptionDetails };
-        } else {
-            user.subscription = {
-                stripe_price_id: null,
-                stripe_subscription_id: null,
-                start_date: null,
-                next_payment_date: null,
-                payment_method: null,
-                invoice_history: [],
-            }
-        }
-
-        data = clearSensitiveData({ ...user });
-        jwt = signAuthJwt({ email: user.email, id: user.id });
-    } else {
-        const user = resultA[0];
-
-        if (user.disabled) {
-            user.disabled = false;
-            user.alert_email = true;
-            user.alert_text = !!user.phone;
-            user.alert_browser_notification = true;
-            user.alert_push_notification = true;
-
-            const valuesB = [user.disabled, user.alert_email, user.alert_text, user.alert_browser_notification, user.alert_push_notification, user.id];
-            const queryB = "UPDATE user SET disabled=?, alert_email=?, alert_text=?, alert_browser_notification=?, alert_push_notification=? WHERE id=?";
-            await Database.execute(queryB, valuesB);
-        }
-
-        const valuesC = [user.id, subscriptionActive];
-        const queryC = "SELECT p.stripe_price_id, s.stripe_subscription_id FROM subscription s, plan p WHERE s.user_id=? AND s.status_id=? AND s.plan_id=p.id";
-        const [resultC] = await Database.execute(queryC, valuesC);
-
-        if (resultC.length > 0) {
-            user.subscription = resultC[0];
-
-            const subscriptionDetails = await getSubscriptionDetails(user.subscription.stripe_subscription_id);
-            user.subscription = { ...user.subscription, ...subscriptionDetails };
-        } else {
-            user.subscription = {
-                stripe_price_id: null,
-                stripe_subscription_id: null,
-                start_date: null,
-                next_payment_date: null,
-                payment_method: null,
-                invoice_history: [],
-            }
-        }
-
-        data = clearSensitiveData({ ...user });
-        jwt = signAuthJwt({ email: user.email, id: user.id });
+        res.status(404).json({ data: null, msg: "User not found." });
+        return;
     }
+
+    const user = resultA[0];
+
+    if (user.disabled) {
+        user.disabled = false;
+        user.alert_email = true;
+        user.alert_text = !!user.phone;
+        user.alert_browser_notification = true;
+        user.alert_push_notification = true;
+
+        const valuesB = [user.disabled, user.alert_email, user.alert_text, user.alert_browser_notification, user.alert_push_notification, user.id];
+        const queryB = "UPDATE user SET disabled=?, alert_email=?, alert_text=?, alert_browser_notification=?, alert_push_notification=? WHERE id=?";
+        await Database.execute(queryB, valuesB);
+    }
+
+    const valuesC = [user.id, subscriptionActive];
+    const queryC = "SELECT p.stripe_price_id, s.stripe_subscription_id FROM subscription s, plan p WHERE s.user_id=? AND s.status_id=? AND s.plan_id=p.id";
+    const [resultC] = await Database.execute(queryC, valuesC);
+
+    if (resultC.length > 0) {
+        user.subscription = resultC[0];
+
+        const subscriptionDetails = await getSubscriptionDetails(user.subscription.stripe_subscription_id);
+        user.subscription = { ...user.subscription, ...subscriptionDetails };
+    } else {
+        user.subscription = {
+            stripe_price_id: null,
+            stripe_subscription_id: null,
+            start_date: null,
+            next_payment_date: null,
+            payment_method: null,
+            invoice_history: [],
+        }
+    }
+
+    const data = clearSensitiveData({ ...user });
+    const jwt = signAuthJwt({ email: user.email, id: user.id });
 
     res.cookie("jwt", jwt, { httpOnly: true, secure: SHOPTRACKER_FRONT_HTTPSECURE, sameSite: cookiesSameSite, domain: SHOPTRACKER_FRONT_DOMAIN, maxAge: jwtExpirationTime });
     res.status(200).json({ data: data, msg: "User successfully logged in." });
