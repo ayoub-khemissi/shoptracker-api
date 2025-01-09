73d653df7ee18accb47b1bb1c1b60d13bba0810c
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-01-09 03:19:10 +0100

    feat: Track routes change track_check to track_check_ok table name

--- Routes/TracksStats.js
+++ Routes/TracksStats.js
@@ -7,7 +7,7 @@ const { trackStatusEnabled } = Constants;
 api.get("/track/stats", async function (req, res) {
     const valuesA = [trackStatusEnabled];
     const queryA =
-        "SELECT (SELECT COUNT(*) FROM track WHERE status_id=?) total_tracks_enabled, (SELECT COUNT(*) FROM track_check) total_track_checks";
+        "SELECT (SELECT COUNT(*) FROM track WHERE status_id=?) total_tracks_enabled, (SELECT COUNT(*) FROM track_check_ok) total_track_checks";
     const [resultA] = await Database.execute(queryA, valuesA);
 
     res.status(200).json({ data: resultA[0], msg: "Track stats successfully found." });
