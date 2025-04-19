d01c223f38fa996d7618e65ac36dc005ebd1a202
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-04-19 11:33:43 +0200

    fix: Checkout session route prevent user from buying the same subscription

--- Routes/CheckoutSession.js
+++ Routes/CheckoutSession.js
@@ -2,6 +2,9 @@ import api from "../Modules/Api.js";
 import { extractJwt, verifyAuthJwt } from "../Modules/Auth.js";
 import Database from "../Modules/Database.js";
 import { createCheckoutSession, createCustomer, retrievePrice } from "../Modules/Stripe.js";
+import Constants from "../Utils/Constants.js";
+
+const { subscriptionActive } = Constants;
 
 api.post("/checkout/session", async function (req, res) {
     const jwt = verifyAuthJwt(extractJwt(req.cookies));
@@ -39,21 +42,34 @@ api.post("/checkout/session", async function (req, res) {
 
     const user = resultA[0];
 
+    const valuesB = [jwt.id, subscriptionActive];
+    const queryB = "SELECT stripe_price_id FROM plan WHERE id=(SELECT plan_id FROM subscription WHERE user_id=? AND status_id=?)";
+    const [resultB] = await Database.execute(queryB, valuesB);
+
+    if (resultB.length > 0) {
+        const { stripe_price_id } = resultB[0];
+
+        if (stripe_price_id === stripePriceId) {
+            res.status(400).json({ data: null, msg: "This subscription is already active." });
+            return;
+        }
+    }
+
     if (!user.stripe_customer_id) {
         const customer = await createCustomer({ email: user.email });
         user.stripe_customer_id = customer.id;
 
-        const valuesB = [customer.id, jwt.id];
-        const queryB = "UPDATE user SET stripe_customer_id=? WHERE id=?";
-        await Database.execute(queryB, valuesB);
+        const valuesC = [customer.id, jwt.id];
+        const queryC = "UPDATE user SET stripe_customer_id=? WHERE id=?";
+        await Database.execute(queryC, valuesC);
     }
 
-    const valuesC = [jwt.id];
-    const queryC =
+    const valuesD = [jwt.id];
+    const queryD =
         "SELECT MIN(created_at) AS first_subscription_date FROM subscription WHERE user_id=?";
-    const [resultC] = await Database.execute(queryC, valuesC);
+    const [resultD] = await Database.execute(queryD, valuesD);
 
-    const { first_subscription_date } = resultC[0];
+    const { first_subscription_date } = resultD[0];
     const isFirstSubscription = first_subscription_date === null;
 
     const session = await createCheckoutSession(
@@ -68,7 +84,7 @@ api.post("/checkout/session", async function (req, res) {
     }
 
     res.status(200).json({
-        data: { sessionId: session.id },
+        data: session.id,
         msg: "Checkout session successfully created.",
     });
 });
