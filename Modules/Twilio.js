676715673a3ffe81bb2fd4ca12aed87fd7db1148
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-04-22 18:45:56 +0200

    fix: Twilio module send WhatsApp message

--- Modules/Twilio.js
+++ Modules/Twilio.js
@@ -4,22 +4,25 @@ import Log from "./Log.js";
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER } = Config;
 
-const fromWhatsAppNumber = `whatsapp:${TWILIO_PHONE_NUMBER}`;
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 /**
  * Send a WhatsApp message using Twilio
  * @param {string} to - The recipient's WhatsApp number in the format 'whatsapp:+1234567890'
  * @param {string} body - The message body
+ * @returns {Promise<boolean>} - true if the message was sent successfully, false otherwise
  */
-export function sendWhatsAppMessage(to, body) {
+export async function sendWhatsAppMessage(to, body) {
     try {
-        return client.messages.create({
-            from: fromWhatsAppNumber,
-            to: to,
+        await client.messages.create({
+            from: `whatsapp:${TWILIO_PHONE_NUMBER}`,
+            to: `whatsapp:${to}`,
             body: body,
         });
+
+        return true;
     } catch (error) {
         Log.error("An error occurred while sending the WhatsApp message: " + error);
+        return false;
     }
 }
